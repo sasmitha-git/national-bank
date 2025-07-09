20312922d9b763d0a15c8f06fa3e00b578be1a85
Sasmitha <sasmithamadhusara@gmail.com>
2025-07-10 00:26:14 +0530

    login ok customer accounts data show ok Transfer Money ok Transfer History Show ok Schedule Transfer ok

--- business/src/main/java/lk/jiat/bank/ejb/bean/TimerSessionBean.java
+++ business/src/main/java/lk/jiat/bank/ejb/bean/TimerSessionBean.java
@@ -1,52 +1,108 @@
 package lk.jiat.bank.ejb.bean;
 
-
 import jakarta.annotation.Resource;
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.*;
-import lk.jiat.bank.core.dto.Task;
-import lk.jiat.bank.core.service.TaskService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lk.jiat.bank.core.model.ScheduleStatus;
+import lk.jiat.bank.core.model.ScheduledTask;
+import lk.jiat.bank.core.service.AccountService;
+import lk.jiat.bank.core.service.ScheduleService;
 import lk.jiat.bank.core.service.TransactionService;
 
-import java.io.Serializable;
-import java.util.UUID;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Singleton
-public class TimerSessionBean implements TaskService {
+public class TimerSessionBean implements ScheduleService {
 
-    @Resource
-    private TimerService timerService;
+   @Resource
+   private TimerService timerService;
 
-    @EJB
-    private TransactionService transactionService;
+   @EJB
+   private TransactionService transactionService;
+
+   @EJB
+   private AccountService accountService;
+
+   @PersistenceContext
+   private EntityManager em;
+
 
 
     @Override
-    public void scheduleTransfer(Task task,long delayInMilliSeconds) {
+    public Long scheduleTransfer(ScheduledTask scheduledTask, long delayInMillis) {
+
+        scheduledTask.setStatus(ScheduleStatus.ACTIVE);
+        em.persist(scheduledTask);
+        em.flush();
+
+        Long taskId = scheduledTask.getId();
+        if(taskId == null){
+            throw new IllegalStateException("Task id is null");
+        }
+
         TimerConfig timerConfig = new TimerConfig();
-        timerConfig.setInfo(task);
-        timerService.createSingleActionTimer(delayInMilliSeconds, timerConfig);
+        timerConfig.setInfo(scheduledTask.getId());
+        timerService.createSingleActionTimer(delayInMillis, timerConfig);
 
-        String taskId = UUID.randomUUID().toString();
-        System.out.println("Transfer scheduled with Task Id: " + taskId);
-
+        return taskId;
     }
 
     @Timeout
-    public void startScheduleTransfer(Timer timer) {
-        Serializable info = timer.getInfo();
-        if(info instanceof Task) {
-            Task task = (Task) info;
-            System.out.println("Executing Task: " + task.getFromAccount() + " => " + task.getToAccount());
+    public void  executeScheduledTransfer(Timer timer) {
+
+        Long taskId = (Long) timer.getInfo();
+        if(taskId == null){
+            System.out.println("Task id is null");
+            return;
+        }
+
+        ScheduledTask scheduledTask = em.find(ScheduledTask.class, taskId);
+
+        if(scheduledTask != null && scheduledTask.getStatus() == ScheduleStatus.ACTIVE ) {
 
             try{
-                transactionService.transferFunds(task.getFromAccount(), task.getToAccount(), task.getAmount());
+                transactionService.transferFunds(
+                        scheduledTask.getFromAccount().getAccountNumber(),
+                        scheduledTask.getToAccount().getAccountNumber(),
+                        scheduledTask.getAmount()
+                );
+                scheduledTask.setStatus(ScheduleStatus.COMPLETED);
             } catch (Exception e) {
+                scheduledTask.setStatus(ScheduleStatus.CANCELLED);
                 e.printStackTrace();
-                System.out.println("Schedule transfer failed");
             }
+            scheduledTask.setNextExecutionTime(LocalDateTime.now());
+            em.merge(scheduledTask);
+
         }
+
     }
 
 
+    @Override
+    public List<ScheduledTask> getActiveTasks(Long userId) {
+        return em.createNamedQuery("ScheduledTask.findScheduledTaskByUserId",ScheduledTask.class)
+                .setParameter("userId", userId)
+                .setParameter("status", ScheduleStatus.ACTIVE)
+                .getResultList();
+    }
+
+    @Override
+    public void cancelScheduledTask(Long taskId) {
+        ScheduledTask scheduledTask = em.find(ScheduledTask.class, taskId);
+        if(scheduledTask != null && scheduledTask.getStatus() == ScheduleStatus.ACTIVE) {
+            scheduledTask.setStatus(ScheduleStatus.CANCELLED);
+            em.merge(scheduledTask);
+        }
+
+        for(Timer timer : timerService.getTimers()) {
+            if(timer.getInfo() instanceof Long && ((Long)timer.getInfo()).equals(taskId)) {
+                timer.cancel();
+                System.out.println("Timer cancelled"+ taskId);
+                break;
+            }
+        }
+    }
 }
